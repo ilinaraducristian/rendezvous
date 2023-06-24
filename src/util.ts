a07dbef4fc4bac413df874e78e3546b04da8dbe1
ilinaraducristian <ilinaraducristian@proton.me>
2023-06-24 08:44:22 +0300

    Svelte scaffold

--- src/util.ts
+++
@@ -1,9 +0,0 @@
-export function createDebouncedAsyncFunction(fcn: Function) {
-  let executing = false;
-  return async () => {
-    if(executing) return;
-    executing = true;
-    await fcn();
-    executing = false;
-  }
-}
